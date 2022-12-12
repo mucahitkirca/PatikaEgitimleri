3b11449a019a6143a7613d58fb06f23aaa3b7e7e
mucahitkirca <mucokirca@gmail.com>
2022-12-12 23:04:19 +0300

    worked on image view and stack view

---
+++ ImageandStackView/ImageandStackView/Controllers/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ImageandStackView
+//
+//  Created by Mucahit Kirca on 9.12.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
